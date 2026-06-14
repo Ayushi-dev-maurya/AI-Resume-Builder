1c3dcfa0b670194d9e12b0ae3037a7819677b87e
Ayushi-dev-maurya <ayushi30maurya@gmail.com>
2026-06-14 18:50:56 +0530

    Separated HTML CSS and JAVASCRIPT files

---
+++ script.js
@@ -0,0 +1,73 @@
+//  mobile menu
+ const menuBtn = document.querySelector(".menu-btn");
+
+      const navLinks = document.querySelector(".nav-links");
+
+      menuBtn.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+      });
+
+      // Dark Mode
+
+      const themeBtn = document.getElementById("theme-toggle");
+
+      themeBtn.addEventListener("click", () => {
+        document.body.classList.toggle("dark");
+      });
+
+      // FAQ Accordion
+
+      const questions = document.querySelectorAll(".question");
+
+      questions.forEach((q) => {
+        q.addEventListener("click", () => {
+          q.nextElementSibling.classList.toggle("show");
+        });
+      });
+
+      // Counter
+
+      let count = 0;
+
+      const counter = document.getElementById("resumeCount");
+
+      let interval = setInterval(() => {
+        count += 25;
+
+        counter.innerText = count + "+";
+
+        if (count >= 5000) {
+          clearInterval(interval);
+        }
+      }, 20);
+
+      // Contact Form
+
+      document.getElementById("contactForm").addEventListener("submit", (e) => {
+        const email = document.getElementById("email").value;
+
+        if (email === "") {
+          e.preventDefault();
+
+          alert("Please enter email");
+        }
+      });
+
+      // Back To Top
+
+      const topBtn = document.getElementById("topBtn");
+
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+          topBtn.style.display = "block";
+        } else {
+          topBtn.style.display = "none";
+        }
+      });
+
+      topBtn.addEventListener("click", () => {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      });
